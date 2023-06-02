17238f2cfb0f7b16d9474db0c8778bb6d2a4be95
Kyle <kyleslaughter8@gmail.com>
2023-06-01 18:04:50 -0600

    help

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
 const API_KEY = "76b88bf1b17386c11d9dce4da4425290";
+console.log(API_KEY);
 
 $(function () {
+    var searchBar = $('#city-search');
+    console.log(searchBar);
 
+    searchBar.on('change', function () {
+        console.log(searchBar.val());
+    });
 });
